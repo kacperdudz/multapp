58c56c8a3aeb4137c2022a7ed1612f63957377a8
Kacper <kacperdudz@gmail.com>
2021-02-17 19:10:26 -0600

    initial back end commit; base from spring initializr

---
+++ src/main/java/com/kacperdudz/multapp/MultappApplication.java
@@ -0,0 +1,13 @@
+package com.kacperdudz.multapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kacperdudz/multapp/MultappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kacperdudz.multapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
